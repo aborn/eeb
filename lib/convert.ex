6a9202a86769d1699eed00b50d1d6523a898c503
guobao.jiang <loveaborn@foxmail.com>
2015-12-20 00:04:20 +0800

    read blog file list

--- lib/convert.ex
+++ lib/convert.ex
@@ -2,6 +2,22 @@ defmodule Eeb.Convert do
   @moduledoc """
   将markdown文件转化成html文档
   """
+
+  def blog_files() do
+    case File.ls(post_path()) do
+      {:ok, [files]} ->
+        files
+      {:error, reason} ->
+        IO.puts "read error #{reason}"
+        []
+      _ ->
+        []
+    end
+  end
+  
+  @doc """
+  得到博客文章的目录，默认为项目根目录下的posts
+  """
   def post_path() do
     Path.join(__DIR__, "../posts/") |> Path.expand()
   end
--- mix.exs
+++ mix.exs
@@ -29,6 +29,7 @@ defmodule Eeb.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:earmark, "~> 0.1.19", only: :dev}]
+    [{:earmark, "~> 0.1.19", only: :dev},
+    {:ex_doc, "~> 0.11.1", only: :dev}]
   end
 end
