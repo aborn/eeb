18d49fbbcc56d38303c1e9a728ab80ab2a4ad4fc
蒋国宝 <loveaborn@foxmail.com>
2016-01-05 00:03:02 +0800

    index.ex

--- lib/eeb/index.ex
+++ lib/eeb/index.ex
@@ -1,6 +1,13 @@
 defmodule Eeb.Index do
-
+  @filename "index.html"
+  alias Eeb.BlogPath
+  
   @moduledoc """
   处理博客的首页
   """
+
+  def get_index_file_name do
+    Path.join(BlogPath.html_path, @filename)
+  end
+
 end
