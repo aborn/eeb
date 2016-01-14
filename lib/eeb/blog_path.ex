961d076de65e48a1b7f0273c74ade86394fdefe3
蒋国宝 <loveaborn@foxmail.com>
2016-01-14 23:43:28 +0800

    blog_path can be configed by mix eeb.config

--- lib/eeb/blog_path.ex
+++ lib/eeb/blog_path.ex
@@ -2,13 +2,16 @@ defmodule Eeb.BlogPath do
   @moduledoc """
   博客路径处理
   """
+  alias Eeb.ConfigUtils
+  @blog_path_key :blog_path
   
   @doc """
   得到博客文章的目录，默认为项目根目录下的posts
   """
   def post_path() do
     default = Path.join(__DIR__, "../../posts/") |> Path.expand()
-    "/Users/aborn/github/technotebook"
+    config = ConfigUtils.read
+    Keyword.get(config, @blog_path_key, default)
   end
 
   def html_path() do
