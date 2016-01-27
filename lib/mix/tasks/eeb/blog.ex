5eae2f90e0865b51642aea3f516a361e25f79aac
蒋国宝 <loveaborn@foxmail.com>
2016-01-28 01:15:59 +0800

    update eeb.ez

--- lib/mix/tasks/eeb/blog.ex
+++ lib/mix/tasks/eeb/blog.ex
@@ -8,8 +8,8 @@ defmodule Mix.Tasks.Eeb.Blog do
   
   def run(_args) do
     Mix.shell.info "start generate blog.."
-    # dir = Path.join(__DIR__, "./")
-    # Hex.Shell.info("#{dir}")
+    dir = Path.join(__DIR__, "./")
+    Hex.Shell.info("#{dir}")
     Convert.convert_markdown_blogs_to_html()
     Mix.shell.info "finished the task!"
     Image.yank_images_from_origin()
