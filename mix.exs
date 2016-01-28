9949f165dc45e1f7ebcdf85f92757397550a958e
guobao.jiang <loveaborn@foxmail.com>
2016-01-28 12:58:21 +0800

    add eeb mod

--- mix.exs
+++ mix.exs
@@ -18,7 +18,8 @@ defmodule Eeb.Mixfile do
 
   def application do
     [
-     applications: [:logger,:tzdata,:cowboy]
+      applications: [:logger,:tzdata,:cowboy],
+      mod: {Eeb, []}
     ]
   end
 
@@ -46,35 +47,8 @@ defmodule Eeb.Mixfile do
   end
 
   defp aliases do
-    [compile: ["deps.check", &unload_hex/1, "compile"],
-     run: [&unload_hex/1, "run"],
-     install: ["archive.build -o archives/eeb.ez", "archive.install archives/eeb.ez --force"],
+    [install: ["archive.build -o archives/eeb.ez", "archive.install archives/eeb.ez --force"],
      uninstall: ["archive.uninstall eeb.ez"]]
   end
-
-  defp unload_hex(_) do
-    paths = Path.join(Mix.Local.archives_path, "eeb*.ez") |> Path.wildcard
-
-    Enum.each(paths, fn archive ->
-      ebin = Mix.Archive.ebin(archive)
-      Code.delete_path(ebin)
-
-      {:ok, files} = :erl_prim_loader.list_dir(to_char_list(ebin))
-
-      Enum.each(files, fn file ->
-        file = List.to_string(file)
-        size = byte_size(file) - byte_size(".beam")
-
-        case file do
-          <<name :: binary-size(size), ".beam">> ->
-            module = String.to_atom(name)
-            :code.delete(module)
-            :code.purge(module)
-          _ ->
-            :ok
-        end
-      end)
-    end)
-  end
-
+  
 end
