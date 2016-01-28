ed9caf7fd7de333f2c3f10e0ba5f522e7f7e5e3c
guobao.jiang <loveaborn@foxmail.com>
2016-01-28 14:18:14 +0800

    release 0.1.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Eeb.Mixfile do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   
   def project do
     [app: :eeb,
@@ -30,7 +30,7 @@ defmodule Eeb.Mixfile do
   end
 
   defp package do
-    [
+    [ files: ["lib", "html", "installer", "posts", "LICENSE", "README.md"]
       maintainers: ["Aborn Jiang"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/aborn/eeb"}]
