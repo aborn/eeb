8d73aa0b6b90c652748ce27703c8db35aca7719e
蒋国宝 <loveaborn@foxmail.com>
2016-04-18 22:54:44 +0800

    update

--- scripts/config_eeb.sh
+++ scripts/config_eeb.sh
@@ -3,4 +3,6 @@ cd "$(dirname "$0")"
 cd ..
 echo `pwd`
 mix eeb.config blog_port 4001
+mix eeb.config webhook_token eeb
 mix eeb.config duoshuo_short_name "eeb"
+mix eeb.config blog_path "/home/aborn/github/eeb/posts"
