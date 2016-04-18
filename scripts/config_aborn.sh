f5fe81cac20010ed2fa69704f4d8be2998424e5f
蒋国宝 <loveaborn@foxmail.com>
2016-04-18 23:10:48 +0800

    路径

--- scripts/config_aborn.sh
+++ scripts/config_aborn.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 # mix eeb.config blog_path "/Users/aborn/github/technotebook"
+mix eeb.config blog_path "/home/aborn/github/technotebook"
 mix eeb.config blog_name "aborn"
 mix eeb.config blog_slogan "思考创造个性，个性成就自我！"
 mix eeb.config blog_github "https://github.com/aborn/eeb"
